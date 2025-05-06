8b5aa973a0583b8bac03b28787686437259c233b
ANIMESH PANNA <139163533+7wh0Am-i@users.noreply.github.com>
2025-05-06 14:02:48 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hacked by Team Anonymous India</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            background-color: #000;
+            color: #ff0000;
+            font-family: 'Courier New', monospace;
+            text-align: center;
+        }
+        .logo {
+            margin-top: 50px;
+        }
+        .logo img {
+            width: 300px;
+            border-radius: 50%;
+            box-shadow: 0 0 40px red;
+        }
+        h1 {
+            font-size: 3em;
+            margin: 20px 0;
+            text-shadow: 0 0 10px red;
+        }
+        .quote {
+            font-size: 1.3em;
+            margin-top: 30px;
+            color: #fff;
+        }
+        .quote span {
+            display: block;
+            margin: 10px 0;
+        }
+        .footer {
+            margin-top: 50px;
+            font-size: 0.9em;
+            color: #888;
+        }
+    </style>
+</head>
+<body>
+    <div class="logo">
+        <img src="Screenshot 2025-05-06 135839.png" alt="Team Anonymous India Logo">
+    </div>
+    <h1>HACKED BY<br>TEAM ANONYMOUS INDIA</h1>
+    <div class="quote">
+        <span>We Are Anonymous.</span>
+        <span>We Are Legion.</span>
+        <span>We Do Not Forgive.</span>
+        <span>We Do Not Forget.</span>
+        <span>Expect Us.</span>
+    </div>
+    <div class="footer">
+        <p>For demo or educational use only.</p>
+    </div>
+</body>
+</html>
